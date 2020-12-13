19515f71ed101ce63d3a19e4a6bc575aa64b0955
nafisgizatov <el_bruho@mail.ru>
2020-12-13 11:09:14 +0500

    create app

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BattleField from './battle/main';
-import {IUnit} from './battle/factory/unit';
-import UnitFactory from './battle/factory/unit';
-import {ARMY} from './battle/army';
+import Main from './main';
 
-const leftArmy: IUnit[] = [];
-const rightArmy: IUnit[] = [];
 
-ARMY.forEach((item) => {
-    item.units.forEach((unit, index) => {
-        const config = {
-            position: {
-                x: item.side === 'left' ? 1 : 15,
-                y: index + 1
-            },
-            direction: item.side === 'left' ? 'right' : 'left',
-            battleSide: item.side
-        }
-
-        const newUnit = UnitFactory.create(item.fraction, unit.name, config);
-        if (item.side === 'left') {
-            leftArmy.push(newUnit);
-        } else {
-            rightArmy.push(newUnit);
-        }
-    });
-});
-
-ReactDOM.render(<BattleField leftArmy={leftArmy} rightArmy={rightArmy} />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
