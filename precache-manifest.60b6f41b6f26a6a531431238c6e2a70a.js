27a515655570c7f2e0f8414ee7de1f38ff970754
nafisgizatov <el_bruho@mail.ru>
2020-12-13 11:32:34 +0500

    Updates

---
+++ precache-manifest.60b6f41b6f26a6a531431238c6e2a70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310df8881dedf83f47628e12ba9ede3f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "cc95780242dbcf341df2",
+    "url": "/react-app/static/css/main.dd88fa2e.chunk.css"
+  },
+  {
+    "revision": "093d7b0c634264da1eb3",
+    "url": "/react-app/static/js/2.489aaaaf.chunk.js"
+  },
+  {
+    "revision": "362b16cd6e882e07c107d9f3bc303e0c",
+    "url": "/react-app/static/js/2.489aaaaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc95780242dbcf341df2",
+    "url": "/react-app/static/js/main.3a08c6ef.chunk.js"
+  },
+  {
+    "revision": "5d6bfdf656a75dfb279f",
+    "url": "/react-app/static/js/runtime-main.a3aa19e6.js"
+  }
+]);
